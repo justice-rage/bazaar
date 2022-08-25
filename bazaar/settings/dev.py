8e8ff55cb78e3ec3510e5ce1d1e3193b9c5dea75
justicepelteir <justice.douglas@hey.com>
2022-08-25 11:39:53 -0700

    initial commit

---
+++ bazaar/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-joljxnvkj_i!)3fch6usi$uhzy6@(4u_xo0pp7_k(6xjyqrs%z"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
